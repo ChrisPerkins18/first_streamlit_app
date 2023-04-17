0a25912e5db463073255d8d5709a5a92279358aa
ChrisPerkins18 <120106147+ChrisPerkins18@users.noreply.github.com>
2023-04-17 11:32:47 +0100

    Update streamlit_app.py
    Imported pandas and read fruit macros to a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My parents new healthy diner')
 
@@ -10,3 +11,6 @@ streamlit.text(' 🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('🍌🥭 Banana Strawberry smoothie')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
